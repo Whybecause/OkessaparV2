722f74c93230388fb903deb40f0ea3ed2bfcc139
whybecause <maxence.traina@gmail.com>
2025-02-22 16:44:47 +0100

    fix build

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,6 +1,6 @@
 import HomePage from "../home-page";
 
-export const revalidate = Infinity; // Pour que cette page ne soit jamais regénérée côté serveur vu que data change pas -> + rapide
+export const revalidate = false; // Pour que cette page ne soit jamais regénérée côté serveur vu que data change pas -> + rapide
 
 export default function Home() {
   return <HomePage />;
