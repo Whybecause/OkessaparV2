16506834cd7bb790485d1c197fe7d1ed9d9347b9
whybecause <maxence.traina@gmail.com>
2025-02-22 03:07:33 +0100

    add JSON-LD data for google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-import type { Viewport } from 'next'
+import type { Viewport } from "next";
 
 import "./globals.css";
 
@@ -25,11 +26,11 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-}
+};
 
 export default function RootLayout({
   children,
@@ -41,6 +42,46 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          id="json-ld-global"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "MusicGroup",
+              name: "Okessapar",
+              url: "https://okessapar.com",
+              sameAs: [
+                "https://www.instagram.com/okessapar",
+                "https://www.facebook.com/okessapar",
+                "https://open.spotify.com/artist/1n8P1ZPMaY61KWcfTG7l1Z",
+                "https://music.apple.com/artist/1482952367",
+              ],
+              image: "https://okessapar.com/logo.png",
+              description: "Une voix singulière, tantôt rappée, tantôt chantée, accompagne des textes en français doux comme percutants.",
+              genre: "Pop slam alternatif",
+              foundingDate: "2019",
+              member: [
+                {
+                  "@type": "Person",
+                  name: "Flavien Moutier",
+                },
+                {
+                  "@type": "Person",
+                  name: "Maxence Traina",
+                },
+                {
+                  "@type": "Person",
+                  name: "Lucas Murzeau",
+                },
+                {
+                  "@type": "Person",
+                  name: "Thomas Codis",
+                },
+              ],
+            }),
+          }}
+        />
         <ToasterProvider />
         <div>
           {children}
